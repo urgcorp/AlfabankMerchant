05b80fb2f8529fad9f6eaee63685b55a75a88af7
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-10-10 18:29:51 +0300

    Define constants to reference for action auth methods

--- alfabank/AuthParams.cs
+++ alfabank/AuthParams.cs
@@ -9,15 +9,19 @@ namespace alfabank
         /// </summary>
         public sealed class AuthMethod : StringEnum<AuthMethod>
         {
+            public const string LOGIN_METHOD = "LOGIN";
+
+            public const string TOKEN_METHOD = "TOKEN";
+
             /// <summary>
             /// Login and password from alfabank
             /// </summary>
-            public static readonly AuthMethod LOGIN = RegisterEnum("LOGIN");
+            public static readonly AuthMethod LOGIN = RegisterEnum(LOGIN_METHOD);
 
             /// <summary>
             /// Payment token from alfabank
             /// </summary>
-            public static readonly AuthMethod TOKEN = RegisterEnum("TOKEN");
+            public static readonly AuthMethod TOKEN = RegisterEnum(TOKEN_METHOD);
 
             /// <summary>
             /// Avaliable authorization methods
