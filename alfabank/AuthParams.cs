3b6135d93f9adaf6895d5b85c244bb75769f266b
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-10-10 18:09:26 +0300

    Add AuthParams
    Way to pass override or pass authorization to client

---
+++ alfabank/AuthParams.cs
@@ -0,0 +1,37 @@
+﻿using alfabank.ComponentModel;
+
+namespace alfabank
+{
+    public class AuthParams
+    {
+        /// <summary>
+        /// Method of authorization when calling actions
+        /// </summary>
+        public sealed class AuthMethod : StringEnum<AuthMethod>
+        {
+            /// <summary>
+            /// Login and password from alfabank
+            /// </summary>
+            public static readonly AuthMethod LOGIN = RegisterEnum("LOGIN");
+
+            /// <summary>
+            /// Payment token from alfabank
+            /// </summary>
+            public static readonly AuthMethod TOKEN = RegisterEnum("TOKEN");
+
+            /// <summary>
+            /// Avaliable authorization methods
+            /// </summary>
+            public static readonly AuthMethod[] AVAILABLE = new[] { LOGIN, TOKEN };
+
+
+            public static implicit operator AuthMethod[](AuthMethod obj) => new[] { obj };
+        }
+
+        public string? Login { get; set; }
+
+        public string? Password { get; set; }
+
+        public string? Token { get; set; }
+    }
+}
