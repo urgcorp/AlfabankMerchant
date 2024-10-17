478dfca6ed1099402509b23f8d0a3273a3fd3bf2
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-10-17 19:49:48 +0300

    Reorganize alfabank services (interfaces)

--- alfabank/Services/IAlfabankOrderingService.cs
+++ alfabank/Services/IAlfabankOrderingService.cs
@@ -1,33 +1,9 @@
 ﻿using alfabank.Abstractions;
 using alfabank.Actions;
-using alfabank.Models;
 using alfabank.Models.Response;
 
-namespace alfabank
+namespace alfabank.Services
 {
-    public interface IAlfabankService : IAlfabankMerchantService, IAlfabankOrderingService
-    {
-        /// <summary>
-        /// Регистрация заказа с предавторизацией
-        /// </summary>
-        Task RegisterOrderPreAuthAsync(RegisterOrderPreAuthAction action, AuthParams? auth = null);
-
-        /// <summary>
-        /// Списание суммы предавторизации (полной или частичной)
-        /// </summary>
-        Task DepositOrderAsync(DepositOrderAction action, AuthParams? auth = null);
-
-        /// <summary>
-        /// Запрос статистики по платежам за период
-        /// </summary>
-        Task<LastOrdersForMerchants> GetLastOrdersForMerchantAsync(GetLastOrdersForMerchantsAction action, AuthParams? auth = null);
-
-        /// <summary>
-        /// Расширенный запрос состояния заказа
-        /// </summary>
-        Task<Order> GetOrderStatusExtendedAsync(GetOrderStatusExtendedAction action, AuthParams? auth = null);
-    }
-
     /// <summary>
     /// Service that provides ability to register new orders
     /// <para>Only method that can be called without permissin (Requires merchant token)</para>
---
+++ alfabank/Services/IAlfabankService.cs
@@ -0,0 +1,30 @@
+﻿using alfabank.Abstractions;
+using alfabank.Actions;
+using alfabank.Models;
+using alfabank.Models.Response;
+
+namespace alfabank.Services
+{
+    public interface IAlfabankService : IAlfabankMerchantService, IAlfabankOrderingService
+    {
+        /// <summary>
+        /// Регистрация заказа с предавторизацией
+        /// </summary>
+        Task RegisterOrderPreAuthAsync(RegisterOrderPreAuthAction action, AuthParams? auth = null);
+
+        /// <summary>
+        /// Списание суммы предавторизации (полной или частичной)
+        /// </summary>
+        Task DepositOrderAsync(DepositOrderAction action, AuthParams? auth = null);
+
+        /// <summary>
+        /// Запрос статистики по платежам за период
+        /// </summary>
+        Task<LastOrdersForMerchants> GetLastOrdersForMerchantAsync(GetLastOrdersForMerchantsAction action, AuthParams? auth = null);
+
+        /// <summary>
+        /// Расширенный запрос состояния заказа
+        /// </summary>
+        Task<Order> GetOrderStatusExtendedAsync(GetOrderStatusExtendedAction action, AuthParams? auth = null);
+    }
+}
