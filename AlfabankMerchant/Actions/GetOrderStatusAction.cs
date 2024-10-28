eae3c268dcf40d3f34722541eac24a118cdc439d
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-10-29 01:08:22 +0300

    Create GetOrderStatusAction.cs

---
+++ AlfabankMerchant/Actions/GetOrderStatusAction.cs
@@ -0,0 +1,28 @@
+﻿using AlfabankMerchant.ComponentModel;
+using AlfabankMerchant.Models;
+
+namespace AlfabankMerchant.Actions
+{
+    /// <summary>
+    /// Запрос получения текущего состояния заказа
+    /// </summary>
+    [LoginAuthorization]
+    [RestUrl("rest/getOrderStatus.do")]
+    [WSUrl("soap/merchant-ws")]
+    public class GetOrderStatusAction : AlfabankAction<Order>
+    {
+        /// <summary>
+        /// <para>Номер заказа в платёжной системе</para>
+        /// <para>Уникален в пределах системы</para>
+        /// </summary>
+        [ActionProperty("orderId", Type = "ANS36")]
+        public string? OrderId { get; set; }
+
+        /// <summary>
+        /// <para>Язык в кодировке ISO 639-1. Если не указан, считается, что язык – русский</para>
+        /// <para>Сообщение ошибке будет возвращено именно на этом языке</para>
+        /// </summary>
+        [ActionProperty("language", Type = "A2")]
+        public string? Language { get; set; }
+    }
+}
