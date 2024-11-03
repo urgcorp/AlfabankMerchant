6d3c7b1c6d4b1b28400ade669cada1e4eef07e1a
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-11-03 05:25:41 +0300

    Add AlfabankOperationCallback model

---
+++ AlfabankMerchant/Models/AlfabankOperationCallback.cs
@@ -0,0 +1,113 @@
+﻿using Newtonsoft.Json;
+using static AlfabankMerchant.Models.AlfabankOperationCallback;
+
+namespace AlfabankMerchant.Models
+{
+    public class AlfabankOperationCallback
+    {
+        public struct OperationCallbackParameter
+        {
+            public string Name { get; init; }
+
+            public string Value { get; init; }
+
+            public OperationCallbackParameter(string name, string value)
+            {
+                Name = name;
+                Value = value;
+            }
+
+            public override string ToString()
+            {
+                return $"{Name};{Value}";
+            }
+        }
+
+        /// <summary>
+        /// Уникальный номер заказа в платёжной системе
+        /// </summary>
+        [JsonProperty("mdOrder")]
+        public string OrderId { get; set; }
+
+        /// <summary>
+        /// Уникальный номер (идентификатор) заказа в системе магазина
+        /// </summary>
+        [JsonProperty("orderNumber")]
+        public string OrderNumber { get; set; }
+
+        /// <summary>
+        /// Аутентификационный код, или контрольная сумма, полученная из набора параметров
+        /// </summary>
+        [JsonProperty("checksum")]
+        public string? Checksum { get; set; }
+
+        /// <summary>
+        /// Тип операции, о которой пришло уведомление
+        /// </summary>
+        [JsonProperty("operation")]
+        public CallbackOperationType Operation { get; set; }
+
+        /// <summary>
+        /// Индикатор успешности операции, указанной в параметре <see cref="Operation"/>
+        /// </summary>
+        [JsonProperty("status")]
+        public bool Status { get; set; }
+
+        [JsonProperty("amount")]
+        public int? Amount { get; set; }
+
+        protected AlfabankOperationCallback()
+        { }
+
+        public AlfabankOperationCallback(string mdOrder, string orderNumber, string operation, int status, int? amount = null, string? checksum = null)
+        {
+            Operation = CallbackOperationType.Parse(operation);
+
+            if (status == 0)
+                Status = false;
+            else if (status == 1)
+                Status = true;
+            else throw new ArgumentOutOfRangeException(nameof(status));
+
+            OrderId = mdOrder;
+            OrderNumber = orderNumber;
+            Checksum = checksum;
+            Amount = amount;
+        }
+
+        public OperationCallbackParameter[] GetParameters()
+        {
+            List<OperationCallbackParameter> items = new()
+            {
+                new OperationCallbackParameter("mdOrder", OrderId),
+                new OperationCallbackParameter("orderNumber", OrderNumber),
+                new OperationCallbackParameter("operation", Operation.ToString()),
+                new OperationCallbackParameter("status", Status ? "1" : "0")
+            };
+            if (Amount.HasValue)
+                items.Add(new OperationCallbackParameter("amount", Amount.Value.ToString()));
+
+            return items.ToArray();
+        }
+    }
+
+    public static class OperationCallbackExtension
+    {
+        public static string ToString(this IEnumerable<OperationCallbackParameter> parameters)
+        {
+            return string.Join(";", parameters.OrderBy(x => x.Name));
+        }
+
+        public static bool ValidateChecksum(this IEnumerable<OperationCallbackParameter> parameters, string? checksum, Func<string, string> checksumCalc, out string actualChecksum)
+        {
+            var paramsString = parameters.ToString();
+            actualChecksum = checksumCalc(paramsString!);
+            return checksum == actualChecksum;
+        }
+
+        public static bool ValidateChecksum(this AlfabankOperationCallback callback, Func<string, string> checksumCalc, out string actualChecksum)
+        {
+            return callback.GetParameters().ValidateChecksum(callback.Checksum, checksumCalc, out actualChecksum);
+        }
+    }
+}
---
+++ AlfabankMerchant/Models/CallbackOperationType.cs
@@ -0,0 +1,35 @@
+﻿using AlfabankMerchant.ComponentModel;
+
+namespace AlfabankMerchant.Models
+{
+    /// <summary>
+    /// Тип операции, о которой пришло уведомление
+    /// </summary>
+    public class CallbackOperationType : StringEnum<CallbackOperationType>
+    {
+        /// <summary>
+        /// Операция удержания (холдирования) суммы
+        /// </summary>
+        public static readonly CallbackOperationType Approved = RegisterEnum("approved");
+
+        /// <summary>
+        /// Операция завершения
+        /// </summary>
+        public static readonly CallbackOperationType Deposited = RegisterEnum("deposited");
+
+        /// <summary>
+        /// Операция отмены
+        /// </summary>
+        public static readonly CallbackOperationType Reversed = RegisterEnum("reversed");
+
+        /// <summary>
+        /// Операция возврата
+        /// </summary>
+        public static readonly CallbackOperationType Refunded = RegisterEnum("refunded");
+
+        /// <summary>
+        /// Истекло время, отпущенное на оплату заказа
+        /// </summary>
+        public static readonly CallbackOperationType Timeout = RegisterEnum("declinedByTimeout");
+    }
+}
