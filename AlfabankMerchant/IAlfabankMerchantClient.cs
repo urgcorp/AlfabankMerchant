430cef6dbbeaf378be3951b87f9c9bc5542c8c30
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-11-05 13:43:32 +0300

    Remove CallActionRawAsync from IAlfabankMerchantClient

--- AlfabankMerchant/IAlfabankMerchantClient.cs
+++ AlfabankMerchant/IAlfabankMerchantClient.cs
@@ -6,21 +6,14 @@ namespace AlfabankMerchant
     public interface IAlfabankMerchantClient : IAlfabankMerchant
     {
         /// <summary>
-        /// Make call to server and return raw response (if HTTP Response is OK)
-        /// </summary>
-        /// <param name="action">Action request</param>
-        /// <returns>Response body</returns>
-        /// <exception cref="HttpRequestException"></exception>
-        Task<string> CallActionRawAsync(AlfabankAction action, AuthParams? authentication = null);
-
-        /// <summary>
         /// Make call to server and return deserialized response or throw exception if error reported
         /// </summary>
         /// <typeparam name="TResponse">Response type</typeparam>
         /// <param name="action">Action request</param>
         /// <returns>Deserialized response</returns>
         /// <exception cref="AlfabankException"></exception>
-        Task<TResponse> CallActionAsync<TResponse>(AlfabankAction<TResponse> action, AuthParams? authentication = null) where TResponse : class;
+        Task<TResponse> CallActionAsync<TResponse>(AlfabankAction<TResponse> action, AuthParams? authentication = null)
+            where TResponse : class;
     }
 
     public interface IAlfabankMerchantClient<TConfig> : IAlfabankMerchantClient
