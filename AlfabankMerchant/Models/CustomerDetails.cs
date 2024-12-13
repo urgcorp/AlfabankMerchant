f1fdd5d21ad63efcf504fae8b5d6f26f2a9af9d4
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-12-13 09:26:55 +0300

    Create CustomerDetails.cs

---
+++ AlfabankMerchant/Models/CustomerDetails.cs
@@ -0,0 +1,62 @@
+﻿using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace AlfabankMerchant.Models
+{
+    public class CustomerDetails
+    {
+        /// <summary>
+        /// Электронная почта покупателя.
+        /// </summary>
+        [JsonProperty("email")]
+        [JsonPropertyName("email")]
+        public string? Email { get; set; }
+
+        /// <summary>
+        /// <para>Номер телефона покупателя.</para>
+        /// <para>Если в телефон включён код страны, номер должен начинаться со знака плюс («+»).</para>
+        /// <para>Если телефон передаётся без знака плюс («+»), то код страны указывать не следует.</para>
+        /// </summary>
+        [JsonProperty("phone")]
+        [JsonPropertyName("phone")]
+        public string? Phone { get; set; }
+
+        /// <summary>
+        /// <para>Фамилия, имя и отчество плательщика.</para>
+        /// Параметр возвращается только в том случае, если был передан партнёром при регистрации.
+        /// </summary>
+        [JsonProperty("fullName")]
+        [JsonPropertyName("fullName")]
+        public string? FullName { get; set; }
+
+        /// <summary>
+        /// Серия и номер паспорта плательщика в следующем формате: 2222888888.
+        /// <para>Только для запросов по платежам с фискализацией.</para>
+        /// </summary>
+        [JsonProperty("passport")]
+        [JsonPropertyName("passport")]
+        public string? Passport { get; set; }
+
+        /// <summary>
+        /// Идентификационный номер налогоплательщика.
+        /// <para>Допускается передавать 10 или 12 символов.</para>
+        /// </summary>
+        [JsonProperty("inn")]
+        [JsonPropertyName("inn")]
+        public long? INN { get; set; }
+
+        /// <summary>
+        /// Способ связи с покупателем.
+        /// </summary>
+        [JsonProperty("contact")]
+        [JsonPropertyName("contact")]
+        public string? Contact { get; set; }
+
+        /// <summary>
+        /// Блок с атрибутами адреса для доставки.
+        /// </summary>
+        [JsonProperty("deliveryInfo")]
+        [JsonPropertyName("deliveryInfo")]
+        public string? DeliveryInfo { get; set; }
+    }
+}
