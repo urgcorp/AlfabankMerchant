74a802162365c28b91a313bd9a7a0245cda691c0
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-12-14 01:02:22 +0300

    Add BooleanStringConverter's

---
+++ AlfabankMerchant/JsonConverter/BooleanStringConverter.cs
@@ -0,0 +1,41 @@
+﻿using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace AlfabankMerchant.JsonConverter
+{
+    public class BooleanStringConverter : JsonConverter<bool?>
+    {
+        public override bool? Read(ref Utf8JsonReader reader, Type typeToConvert, JsonSerializerOptions options)
+        {
+            if (reader.TokenType == JsonTokenType.String)
+            {
+                var value = reader.GetString();
+                if (value == null)
+                    return null;
+                if (value.Equals("true", StringComparison.OrdinalIgnoreCase))
+                    return true;
+                if (value.Equals("false", StringComparison.OrdinalIgnoreCase))
+                    return false;
+
+                throw new JsonException("Expected values are 'true', 'false' or null.");
+            }
+            throw new JsonException("Expected string value.");
+        }
+
+        public override void Write(Utf8JsonWriter writer, bool? value, JsonSerializerOptions options)
+        {
+            switch (value)
+            {
+                case true:
+                    writer.WriteStringValue("true");
+                    break;
+                case false:
+                    writer.WriteStringValue("false");
+                    break;
+                default:
+                    writer.WriteNullValue();
+                    break;
+            }
+        }
+    }
+}
---
+++ AlfabankMerchant/JsonConverter/Newtonsoft/BooleanStringConverter.cs
@@ -0,0 +1,39 @@
+﻿using Newtonsoft.Json;
+
+namespace AlfabankMerchant.JsonConverter.Newtonsoft
+{
+    public class BooleanStringConverter : JsonConverter<bool?>
+    {
+        public override bool? ReadJson(JsonReader reader, Type objectType, bool? existingValue, bool hasExistingValue, JsonSerializer serializer)
+        {
+            if (reader.Value is string val)
+            {
+                if (val == null)
+                    return null;
+                if (val.Equals("true", StringComparison.OrdinalIgnoreCase))
+                    return true;
+                if (val.Equals("false", StringComparison.OrdinalIgnoreCase))
+                    return false;
+
+                throw new JsonException("Expected values are 'true', 'false' or null.");
+            }
+            throw new JsonException("Expected string value.");
+        }
+
+        public override void WriteJson(JsonWriter writer, bool? value, JsonSerializer serializer)
+        {
+            switch (value)
+            {
+                case true:
+                    writer.WriteValue("true");
+                    break;
+                case false:
+                    writer.WriteValue("false");
+                    break;
+                default:
+                    writer.WriteNull();
+                    break;
+            }
+        }
+    }
+}
