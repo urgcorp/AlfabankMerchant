e24fbd2fcc720cb28e688e55191e0b456f70df6c
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-12-13 09:26:48 +0300

    Fix PaymentWay.cs

--- AlfabankMerchant/Common/PaymentWay.cs
+++ AlfabankMerchant/Common/PaymentWay.cs
@@ -1,5 +1,5 @@
-﻿using AlfabankMerchant.ComponentModel;
-using AlfabankMerchant.Models;
+﻿using System.Diagnostics;
+using AlfabankMerchant.ComponentModel;
 
 namespace AlfabankMerchant.Common
 {
@@ -16,95 +16,100 @@ namespace AlfabankMerchant.Common
         /// <summary>
         /// Оплата с вводом карточных данных
         /// </summary>
-        public static readonly PaymentWay CARD = RegisterEnum("CARD");
+        public static readonly PaymentWay CARD = RegisterPW("CARD");
 
         /// <summary>
         /// Оплата связкой
         /// </summary>
-        public static readonly PaymentWay CARD_BINDING = RegisterEnum("CARD_BINDING");
+        public static readonly PaymentWay CARD_BINDING = RegisterPW("CARD_BINDING");
 
         /// <summary>
         /// Оплата с помощью "Альфа-Клик" (через систему PayByClik)
         /// </summary>
-        public static readonly PaymentWay ALFA_ALFACLICK = RegisterEnum("ALFA_ALFACLICK");
+        public static readonly PaymentWay ALFA_ALFACLICK = RegisterPW("ALFA_ALFACLICK");
 
         /// <summary>
         /// Оплата платежной системой AlfaPay
         /// </summary>
-        public static readonly PaymentWay ALFAPAY = RegisterEnum("ALFAPAY");
+        public static readonly PaymentWay ALFAPAY = RegisterPW("ALFAPAY");
 
-        public static readonly PaymentWay FILE_BINDING = RegisterEnum("FILE_BINDING");
+        /// <summary>
+        /// Оплата через Систему Быстрых Платежей
+        /// </summary>
+        public static readonly PaymentWay SBP = RegisterPW("SBP");
 
-        public static readonly PaymentWay FILE_SBP_C2B_BINDING = RegisterEnum("FILE_SBP_C2B_BINDING");
+        public static readonly PaymentWay FILE_BINDING = RegisterPW("FILE_BINDING");
 
-        public static readonly PaymentWay P2P = RegisterEnum("P2P");
+        public static readonly PaymentWay FILE_SBP_C2B_BINDING = RegisterPW("FILE_SBP_C2B_BINDING");
 
-        public static readonly PaymentWay P2P_BINDING = RegisterEnum("P2P_BINDING");
+        public static readonly PaymentWay P2P = RegisterPW("P2P");
+
+        public static readonly PaymentWay P2P_BINDING = RegisterPW("P2P_BINDING");
 
         public static readonly PaymentWay[] P2P_PAYMENTS = new[] { P2P, P2P_BINDING };
 
-        public static readonly PaymentWay PAYPAL = RegisterEnum("PAYPAL");
+        public static readonly PaymentWay PAYPAL = RegisterPW("PAYPAL");
 
-        public static readonly PaymentWay APPLE_PAY = RegisterEnum("APPLE_PAY");
+        public static readonly PaymentWay APPLE_PAY = RegisterPW("APPLE_PAY");
 
-        public static readonly PaymentWay APPLE_PAY_BINDING = RegisterEnum("APPLE_PAY_BINDING");
+        public static readonly PaymentWay APPLE_PAY_BINDING = RegisterPW("APPLE_PAY_BINDING");
 
-        public static readonly PaymentWay GOOGLE_PAY_CARD = RegisterEnum("GOOGLE_PAY_CARD");
+        public static readonly PaymentWay GOOGLE_PAY_CARD = RegisterPW("GOOGLE_PAY_CARD");
 
-        public static readonly PaymentWay GOOGLE_PAY_RAW = RegisterEnum("GOOGLE_PAY_RAW");
+        public static readonly PaymentWay GOOGLE_PAY_RAW = RegisterPW("GOOGLE_PAY_RAW");
 
-        public static readonly PaymentWay GOOGLE_PAY_CARD_BINDING = RegisterEnum("GOOGLE_PAY_CARD_BINDING");
+        public static readonly PaymentWay GOOGLE_PAY_CARD_BINDING = RegisterPW("GOOGLE_PAY_CARD_BINDING");
 
-        public static readonly PaymentWay GOOGLE_PAY_TOKENIZED = RegisterEnum("GOOGLE_PAY_TOKENIZED");
+        public static readonly PaymentWay GOOGLE_PAY_TOKENIZED = RegisterPW("GOOGLE_PAY_TOKENIZED");
 
-        public static readonly PaymentWay GOOGLE_PAY_TOKENIZED_BINDING = RegisterEnum("GOOGLE_PAY_TOKENIZED_BINDING");
+        public static readonly PaymentWay GOOGLE_PAY_TOKENIZED_BINDING = RegisterPW("GOOGLE_PAY_TOKENIZED_BINDING");
 
         public static readonly PaymentWay[] GOOGLE_PAY_PAYMENTS = new[] { GOOGLE_PAY_CARD, GOOGLE_PAY_RAW, GOOGLE_PAY_CARD_BINDING, GOOGLE_PAY_TOKENIZED, GOOGLE_PAY_TOKENIZED_BINDING };
 
-        public static readonly PaymentWay SAMSUNG_PAY = RegisterEnum("SAMSUNG_PAY");
+        public static readonly PaymentWay SAMSUNG_PAY = RegisterPW("SAMSUNG_PAY");
 
-        public static readonly PaymentWay SAMSUNG_PAY_BINDING = RegisterEnum("SAMSUNG_PAY_BINDING");
+        public static readonly PaymentWay SAMSUNG_PAY_BINDING = RegisterPW("SAMSUNG_PAY_BINDING");
 
-        public static readonly PaymentWay SAMSUNG_PAY_RAW = RegisterEnum("SAMSUNG_PAY_RAW");
+        public static readonly PaymentWay SAMSUNG_PAY_RAW = RegisterPW("SAMSUNG_PAY_RAW");
 
-        public static readonly PaymentWay SENDY = RegisterEnum("SENDY");
+        public static readonly PaymentWay SENDY = RegisterPW("SENDY");
 
-        public static readonly PaymentWay SBP_C2B = RegisterEnum("SBP_C2B");
+        public static readonly PaymentWay SBP_C2B = RegisterPW("SBP_C2B");
 
-        public static readonly PaymentWay SBP_C2B_BINDING = RegisterEnum("SBP_C2B_BINDING");
+        public static readonly PaymentWay SBP_C2B_BINDING = RegisterPW("SBP_C2B_BINDING");
 
         /// <summary>
         /// Выплаты СБП
         /// </summary>
-        public static readonly PaymentWay SBP_B2C = RegisterEnum("SBP_B2C");
+        public static readonly PaymentWay SBP_B2C = RegisterPW("SBP_B2C");
 
         /// <summary>
         /// Оплата СБП (Сервис Быстрых Платежей) для Alfa
         /// </summary>
-        public static readonly PaymentWay ALFA_SBP = RegisterEnum("ALFA_SBP");
+        public static readonly PaymentWay ALFA_SBP = RegisterPW("ALFA_SBP");
 
-        public static readonly PaymentWay TOKEN_PAY = RegisterEnum("TOKEN_PAY");
+        public static readonly PaymentWay TOKEN_PAY = RegisterPW("TOKEN_PAY");
 
-        public static readonly PaymentWay TOKEN_PAY_BINDING = RegisterEnum("TOKEN_PAY_BINDING");
+        public static readonly PaymentWay TOKEN_PAY_BINDING = RegisterPW("TOKEN_PAY_BINDING");
 
-        public static readonly PaymentWay YANDEX_PAY_CARD = RegisterEnum("YANDEX_PAY_CARD");
+        public static readonly PaymentWay YANDEX_PAY_CARD = RegisterPW("YANDEX_PAY_CARD");
 
-        public static readonly PaymentWay YANDEX_PAY_TOKENIZED = RegisterEnum("YANDEX_PAY_TOKENIZED");
+        public static readonly PaymentWay YANDEX_PAY_TOKENIZED = RegisterPW("YANDEX_PAY_TOKENIZED");
 
-        public static readonly PaymentWay YANDEX_PAY_CARD_BINDING = RegisterEnum("YANDEX_PAY_CARD_BINDING");
+        public static readonly PaymentWay YANDEX_PAY_CARD_BINDING = RegisterPW("YANDEX_PAY_CARD_BINDING");
 
-        public static readonly PaymentWay YANDEX_PAY_TOKENIZED_BINDING = RegisterEnum("YANDEX_PAY_TOKENIZED_BINDING");
+        public static readonly PaymentWay YANDEX_PAY_TOKENIZED_BINDING = RegisterPW("YANDEX_PAY_TOKENIZED_BINDING");
 
-        public static readonly PaymentWay MIR_PAY = RegisterEnum("MIR_PAY");
+        public static readonly PaymentWay MIR_PAY = RegisterPW("MIR_PAY");
 
-        public static readonly PaymentWay MIR_PAY_BINDING = RegisterEnum("MIR_PAY_BINDING");
+        public static readonly PaymentWay MIR_PAY_BINDING = RegisterPW("MIR_PAY_BINDING");
 
         /// <summary>
         /// Оплата через сервис Альфа-банка "Подели"
         /// <para>BNPL (Buy Now Pay Later)</para>
         /// </summary>
-        public static readonly PaymentWay PODELI = RegisterEnum("PODELI");
+        public static readonly PaymentWay PODELI = RegisterPW("PODELI");
 
-        private static PaymentWay RegisterEnum(string value) => RegisterEnum(new PaymentWay(value));
+        private static PaymentWay RegisterPW(string value) => RegisterEnum(new PaymentWay(value));
     }
 }
