749ea858f647990b1f736f095b1376fa8f7f9b7b
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-12-14 01:02:38 +0300

    Create NameParamsProperty.cs

---
+++ AlfabankMerchant/ComponentModel/NameParamsProperty.cs
@@ -0,0 +1,28 @@
+﻿using System.Diagnostics;
+using System.Text.Json.Serialization;
+using Newtonsoft.Json;
+
+namespace AlfabankMerchant.ComponentModel
+{
+    [DebuggerDisplay("{Name}")]
+    public struct NameParamsProperty
+    {
+        /// <summary>
+        /// Уникальное имя payment плагина
+        /// <para>v: 28+</para>
+        /// </summary>
+        [JsonProperty("name")]
+        [JsonPropertyName("name")]
+        [JsonInclude]
+        public string Name { get; private set; }
+
+        /// <summary>
+        /// Параметры для конкретного платёжного метода. Параметр представляет собой словарь ключ: значение
+        /// <para>v: 28+</para>
+        /// </summary>
+        [JsonProperty("params")]
+        [JsonPropertyName("params")]
+        [JsonInclude]
+        public string ParamsJson { get; private set; }
+    }
+}
