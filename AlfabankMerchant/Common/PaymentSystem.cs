d07002163be15dfe837d0aa39e0ffd8e53624e49
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2024-12-14 00:34:55 +0300

    Refactor PaymentSystem.cs
    Refactor and add description

--- AlfabankMerchant/Common/PaymentSystem.cs
+++ AlfabankMerchant/Common/PaymentSystem.cs
@@ -1,5 +1,4 @@
 ﻿using AlfabankMerchant.ComponentModel;
-using Newtonsoft.Json.Linq;
 
 namespace AlfabankMerchant.Common
 {
@@ -14,12 +13,21 @@ namespace AlfabankMerchant.Common
 
         public static readonly PaymentSystem MASTERCARD = RegisterEnum(new PaymentSystem("MASTERCARD"));
 
+        public static readonly PaymentSystem MIR = RegisterEnum(new PaymentSystem("MIR"));
+
+        /// <summary>
+        /// American Express
+        /// </summary>
         public static readonly PaymentSystem AMEX = RegisterEnum(new PaymentSystem("AMEX"));
 
+        /// <summary>
+        /// Japan Credit Bureau
+        /// </summary>
         public static readonly PaymentSystem JCB = RegisterEnum(new PaymentSystem("JCB"));
 
+        /// <summary>
+        /// China Union Pay
+        /// </summary>
         public static readonly PaymentSystem CUP = RegisterEnum(new PaymentSystem("CUP"));
-
-        public static readonly PaymentSystem MIR = RegisterEnum(new PaymentSystem("MIR"));
     }
 }
