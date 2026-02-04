1ed9c637e09222ee5e206a37251a9b85513681e4
CaCTuCaTu4ECKuu <CaCTuCaTu4ECKuu@users.noreply.github.com>
2026-02-04 22:34:04 +0100

    CLeanup and refactoring

--- AlfabankMerchant.Test/Security/HmacVerifierTest.cs
+++ AlfabankMerchant.Test/Security/HmacVerifierTest.cs
@@ -4,14 +4,16 @@ namespace AlfabankMerchant.Test.Security;
 
 public class HmacVerifierTest
 {
-    public const string SecretToken = "123";
-    public const string message = "\"amount;1500;mdOrder;ed6f3abf-cea0-427e-\nafdf-0ba43ead124f;operation;deposited;orderNumber;89312;status;1;";
+    public const string TestSecretToken = "12345678";
+    public const string TestCallbackData = "\"amount;1500;mdOrder;ed6f3abf-cea0-427e-\nafdf-0ba43ead124f;operation;deposited;orderNumber;89312;status;1;";
+    public const string TestSignature = "38D69820F981235DE4858A89A9EF3E5999E28EEF9E472AADBA77413BC94C935C";
 
     [Fact]
     public void HmacVerifier_CalculateSignature()
     {
         var verifier = new HmacVerifier();
-        var signature = verifier.CalculateHash(SecretToken, message);
+        var signature = verifier.CalculateHash(TestSecretToken, TestCallbackData);
         Assert.NotNull(signature);
+        Assert.Equal(TestSignature, signature);
     }
 }
